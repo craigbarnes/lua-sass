e26aaf939753377b419d46e91874f0cf7e070b28
Craig Barnes <cr@igbarn.es>
2013-04-27 22:38:38 +0100

    Add some test assertions to make sure that errors return nil, errmsg

--- test.lua
+++ test.lua
@@ -1,4 +1,9 @@
 local sass = require "sass"
 
+-- Basic compilation of valid strings and files should work
 assert(sass.compile "a {color: red}")
 assert(sass.compile_file "test.scss")
+
+-- Errors should return nil without causing fatal exceptions
+assert(not sass.compile "invalid-syntax!")
+assert(not sass.compile_file "non-existant.file")
