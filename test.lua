d50561513bb302c3df56f8c62ba26bf3d8e473f2
Craig Barnes <Cr@igBarn.es>
2014-02-20 03:34:33 +0000

    Improve tests

--- test.lua
+++ test.lua
@@ -1,10 +1,18 @@
 local sass = require "sass"
 
--- Basic compilation of valid strings and files should work
-assert(sass.compile("a {color: red}", "compressed", "map", "inc", "images"))
-assert(sass.compile_file "test.scss")
+do -- Compilation of valid SCSS strings should work
+    local scsstext = "$x: red; a {color: $x / 2}"
+    local expected = "a{color:#800000;}"
+    assert(sass.compile(scsstext, "compressed") == expected)
+end
 
--- Errors should return nil without causing fatal exceptions
+do -- Compilation of valid SCSS files should work
+    local filename = "test.scss"
+    local expected = "p{width:550px;color:#cc5200;border-radius:7px;}"
+    assert(sass.compile_file(filename, "compressed") ==  expected)
+end
+
+--Errors should return nil, rather than terminating
 assert(not sass.compile "invalid-syntax!")
 assert(not sass.compile_file "non-existant.file")
 
