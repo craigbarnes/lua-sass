a113a425b6e2800755379081cb0bc3375bab41c7
Craig Barnes <Cr@igBarn.es>
2014-10-11 07:22:11 +0100

    Fix test.lua

--- test.lua
+++ test.lua
@@ -1,35 +1,41 @@
 local sass = require "sass"
+local compile, compile_file = sass.compile, sass.compile_file
+local assert, write = assert, io.write
+local _ENV = nil
 
 do -- Compilation of valid SCSS strings should work
-    local scsstext = "$x: red; a {color: $x / 2}"
-    local expected = "a{color:#800000;}"
-    assert(sass.compile(scsstext, "compressed") == expected)
+    local scsstext = "$x: red; a {color: $x / 3}"
+    local expected = "a{color:#550000;}"
+    local output = assert(compile(scsstext, "compressed"))
+    assert(output == expected)
 end
 
 do -- Compilation of valid SCSS files should work
     local filename = "test.scss"
-    local expected = "p{width:550px;color:#cc5200;border-radius:7px;}"
-    assert(sass.compile_file(filename, "compressed") ==  expected)
+    local expected = "p{width:550px;color:#cc5500;border-radius:7px;}"
+    local output = assert(compile_file(filename, "compressed"))
+    assert(output == expected)
 end
 
 do -- Source maps should be disabled by default and enabled when specified
-    local scsstext = "\n\n\n$x: red; a {color: $x / 2}"
-    local expected = "a {\n  color: #800000; }\n"
-    local expected_maps = "/* line 4, source string */\n" .. expected
-    assert(sass.compile(scsstext, "nested") == expected)
-    assert(sass.compile(scsstext, "nested", false) == expected)
-    assert(sass.compile(scsstext, "nested", true) == expected_maps)
-    assert(sass.compile(scsstext, "nested", 1) == expected_maps)
+    local scsstext = "\n\n\n$x: red; a {color: $x / 3}"
+    local expected = "a {\n  color: #550000; }\n"
+    local expected_maps = "/* line 4, stdin */\n" .. expected
+    assert(compile(scsstext, "nested") == expected)
+    assert(compile(scsstext, "nested", false) == expected)
+    assert(compile(scsstext, "nested", true) == expected_maps)
+    assert(compile(scsstext, "nested", 1) == expected_maps)
 end
 
 do -- Compilation of UTF-8 input containing multi-byte characters should work
     local scsstext = "$สี: blue; .สีน้ำเงิน {color: $สี / 4}"
     local expected = ".สีน้ำเงิน{color:#000040;}"
-    assert(sass.compile(scsstext, "compressed") == expected)
+    local output = compile(scsstext, "compressed")
+    assert(output == expected)
 end
 
 --Errors should return nil, rather than terminating
-assert(not sass.compile "invalid-syntax!")
-assert(not sass.compile_file "non-existant.file")
+assert(not compile "invalid-syntax!")
+assert(not compile_file "non-existant.file")
 
-print "All tests passed"
+write "All tests passed\n"
